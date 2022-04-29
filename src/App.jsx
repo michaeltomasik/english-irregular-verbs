15673b1a1804d6f9b789dc4417bc357083455b94
Ioannis Mentesidis <51725514+GiannisJOHN@users.noreply.github.com>
2022-04-29 23:48:18 +0200

    basic clean-up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/navbar/navbar'
+
+function App () {
+
+  return (
+    <>
+      <Navbar />
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Navbar () {
+
+    return (
+        <nav>
+            <img src="" alt=""/>
+            <h1>English Irregular Verbs</h1>
+        </nav>
+    )
+}
+
+export default Navbar
